69c73eb275ffb197f940eb2ff0663c462996fc9c
luaschelb <lua.schelb@gmail.com>
2024-04-24 20:03:00 -0300

    Integrado o swagger na aplicação

--- codigo/LocadoraVeiculos/LocadoraVeiculos/Program.cs
+++ codigo/LocadoraVeiculos/LocadoraVeiculos/Program.cs
@@ -8,12 +8,21 @@ namespace LocadoraVeiculos
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddDbContext<LocadoraContext>();
-
+            builder.Services.AddDbContext<LocadoraContext>(); 
             builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
-
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
             app.UseAuthorization();
